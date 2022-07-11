cb04e59256b022e56a1ecace9c2e0dd00178f20c
Sergio Sosa <sergio.sosa@servinformacion.com>
2022-07-10 21:50:33 -0500

    functions

--- pages/model.py
+++ pages/model.py
@@ -8,6 +8,113 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 df = pd.read_csv('pages/data/sentiment_1_0.csv')
 
+def log_reg(text):
+    df_rating = df
+    #df_rating=pd.read_csv('dash_app/pages/data/sentiment_1_0.csv')
+    # clean the tweet and save it on a new column
+
+    def clean_tweet(tweet):
+        new_tweet=' '
+        words = tweet.split()
+        for w in words:
+            wl = w.lower() # word in lower case
+    
+            # replace numbers
+            for number, new_value in tools.replace_numbers_dict.items():
+                wl = str(wl).replace(number,new_value)
+
+            # replace some punctuations mark to keep only words
+            for punctuation, new_value in tools.replace_punctuation_dict.items():
+                wl = wl.replace(punctuation,new_value)
+    
+            # replace accent mark 
+            for accent, new_value in tools.replace_accent_dict.items():
+                wl = wl.replace(accent,new_value)
+
+            if wl not in tools.model_stop_wprds:
+                new_tweet += f'{wl} '
+        return new_tweet[:-1]
+    # clean data
+    df_rating['clean_tweet'] = df_rating['full_text'].apply(clean_tweet)
+    # split data into train and test
+    tweets_train, tweets_test = train_test_split(df_rating, test_size=0.2, random_state=0)    # vectorize data
+    vectorizer = CountVectorizer()
+    x_train_bow = vectorizer.fit_transform(tweets_train['clean_tweet'])
+    y_train_bow = tweets_train['sentiment']
+    # train model
+    lr_model_all = LogisticRegression()
+    lr_model_all.fit(x_train_bow, y_train_bow)
+    # predict
+    text=clean_tweet(text)
+    return lr_model_all.predict(vectorizer.transform([text]))[0]
+
+def clean_tweet(tweet):
+
+    new_tweet = ''
+    words = tweet.split()
+    for w in words:
+        wl = w.lower() # word in lower case
+
+        # replace numbers
+        for number, new_value in tools.replace_numbers_dict.items():
+            wl = str(wl).replace(number,new_value)
+
+        # replace some punctuations mark to keep only words
+        for punctuation, new_value in tools.replace_punctuation_dict.items():
+            wl = wl.replace(punctuation,new_value)
+        # replace accent
+        for accent, new_value in tools.replace_accent_dict.items():
+            wl = wl.replace(accent,new_value)
+
+        # remove stop words
+        if wl not in tools.model_stop_wprds:
+            new_tweet += f'{wl} '
+
+    return new_tweet[:-1]
+
+def text_to_lower(tweet):
+    new_tweet = ''
+    words = tweet.split()
+    for w in words:
+        wl = w.lower() # word in lower case
+        new_tweet += f'{wl} '
+    return new_tweet[:-1]
+
+def remove_numbers(tweet):
+    new_tweet = ''
+    words = tweet.split()
+    for w in words:
+        for number, new_value in tools.replace_numbers_dict.items():
+            w = str(w).replace(number,new_value)
+        new_tweet += f'{w} '
+    return new_tweet[:-1]
+
+def remove_punct(tweet):
+    new_tweet = ''
+    words = tweet.split()
+    for w in words:
+        for punctuation, new_value in tools.replace_punctuation_dict.items():
+            w = w.replace(punctuation,new_value)
+        new_tweet += f'{w} '
+    return new_tweet[:-1]
+
+def remove_accent(tweet):
+    new_tweet = ''
+    words = tweet.split()
+    for w in words:
+        for accent, new_value in tools.replace_accent_dict.items():
+            w = w.replace(accent,new_value)
+        new_tweet += f'{w} '
+    return new_tweet[:-1]
+
+def remove_stopword(tweet):
+    new_tweet = ''
+    words = tweet.split()
+    for w in words:
+        if w not in tools.model_stop_wprds:
+            new_tweet += f'{w} '
+    return new_tweet[:-1]
+
 
 p1='''
 Welcome to the model section. 
